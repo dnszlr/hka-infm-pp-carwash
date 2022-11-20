5ea13ebb9e2e14f9f7b5f083a1d929c4331afcd7
dnszlr <Dennis.Zhukki@gmail.com>
2022-11-20 10:14:32 +0100

    final commit

--- src/model/Car.java
+++ src/model/Car.java
@@ -5,28 +5,12 @@ import basic.RandomGenerator;
 
 public class Car {
 
-    private String brand;
-    private String color;
+    private final String brand;
+    private final String color;
 
     public Car() {
         this.brand = IntTo.brand(RandomGenerator.generate(1, 5));
         this.color = IntTo.color(RandomGenerator.generate(1, 5));
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
     }
 
     @Override
